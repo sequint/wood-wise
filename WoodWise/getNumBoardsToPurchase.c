367ca9ebd50b1ee864f7a4d3ec6a93050f5a2772
stevenquintana <steve.e.quint@gmail.com>
2023-11-29 11:35:55 -0800

    Move function body from header to C file

--- WoodWise/getNumBoardsToPurchase.c
+++ WoodWise/getNumBoardsToPurchase.c
@@ -6,3 +6,16 @@
 //
 
 #include "getNumBoardsToPurchase.h"
+
+int getNumBoardsToPurchase(int boardLengths[], int standardLength) {
+    int numBoardsToPurchase = 0;
+    
+    
+    // Check if there is anything in remainder array
+    // If there is check if any board in it is larger than or equal the current loard length
+    // If there is then subtract current board length from that and delete the remainder length from remainder arr
+    // Otherwise subtract the currrent board length from the standard board length and add one to numBoardsToPurchase
+    // If there is a remainder, add one to remainder array length and add to it
+    
+    return numBoardsToPurchase;
+}
