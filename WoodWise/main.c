9304db0ab36cb1ca0c50be41bf0d32149a7d3596
stevenquintana <steve.e.quint@gmail.com>
2023-11-29 10:05:22 -0800

    Update project description

--- WoodWise/main.c
+++ WoodWise/main.c
@@ -3,6 +3,9 @@
 //  WoodWise
 //
 //  Created by Steven Quintana on 11/20/23.
+//  Description: A C console application that calculate the least amount of wood boards you need to purchase for a project.
+//  The project assumes a fixed thinkness and width to the wood (i.e. 1 inch thick and 1 ft. wide) and treats the length as
+//  the only variable.
 //
 
 #include <stdio.h>
@@ -14,6 +17,7 @@ int getStandardWoodLength(void) {
 
 int main(int argc, const char * argv[]) {
     int standardWoodLength = getStandardWoodLength();
+    //
     
     return 0;
 }
