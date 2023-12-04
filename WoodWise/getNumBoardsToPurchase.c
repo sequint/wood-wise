81e4174811b37bdf82da99c982da6d1ad5ff565c
stevenquintana <steve.e.quint@gmail.com>
2023-12-04 11:33:19 -0800

    Simplify total calculation using math instead of arrays for remainder Remove add and remove functions

--- WoodWise/getNumBoardsToPurchase.c
+++ WoodWise/getNumBoardsToPurchase.c
@@ -13,55 +13,12 @@
 
 int getNumBoardsToPurchase(int boardLengths[], int standardLength, int numBoardsInProj)
 {
-    int *remainderLengthsPtr;
-    int remainderBoardsCount = 0;
-    int numBoardsToPurchase = 0;
-    
-    remainderLengthsPtr = (int*)malloc(remainderBoardsCount * sizeof(int));
+    int totalLengths = 0;
     
     for (int i = 0; i < numBoardsInProj; i++)
     {
-        bool remainderCaseFound = false;
-        
-        // If there are any remainder pieces, check if any board in it is larger than or equal the current loard length
-        if (remainderBoardsCount > 0)
-        {
-            for (int x = 0; x < remainderBoardsCount; x++)
-            {
-                // If a remainder piece is found that is longer than current board length
-                // replace the remainder piece length with the difference
-                if (remainderLengthsPtr[x] > boardLengths[i])
-                {
-                    remainderLengthsPtr[x] = remainderLengthsPtr[x] - boardLengths[i];
-                    remainderCaseFound = true;
-                    break;
-                }
-                
-                // If remainder piece equals current board length remove from the remainder arr
-                if (remainderLengthsPtr[x] == boardLengths[i]) {
-                    remainderBoardsCount--;
-                    removeFromArray(remainderLengthsPtr, x, remainderBoardsCount);
-                    remainderCaseFound = true;
-                    break;
-                }
-            }
-        }
-        
-        if (!remainderCaseFound)
-        {
-            // Since no remainder pieces could be used, this must be a new board that is needed
-            numBoardsToPurchase++;
-            
-            int remainder = standardLength - boardLengths[i];
-            if (remainder > 0)
-            {
-                remainderBoardsCount++;
-                addToArray(remainderLengthsPtr, remainderBoardsCount, remainder);
-            }
-        }
+        totalLengths += boardLengths[i];
     }
     
-    free(remainderLengthsPtr);
-    
-    return numBoardsToPurchase;
+    return totalLengths / standardLength + 1;
 }
